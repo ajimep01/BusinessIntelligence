0bad5b091c7020d2e2e83c5c55f1bd8ed73ab49e
Adolfo <ajimep01@estudiantes.unileon.es>
2019-01-03 18:27:24 +0100

    Subida de archivos correcta. Muestra el archivo subido a IPFS

--- client/src/App.js
+++ client/src/App.js
@@ -63,7 +63,7 @@ class App extends Component {
             <div className="pure-u-1-1">
               <h1>Your Image</h1>
               <p>This image is stored on IPFS & The Ethereum Blockchain!</p>
-              <img src="" alt=""/>
+              <img src={`https://ipfs.io/ipfs/${this.state.ipfsHash}`} alt=""/>
               <h2>Upload Image</h2>
               <form onSubmit={this.onSubmit} >
                 <input type='file' onChange={this.captureFile} />
